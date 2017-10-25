c581a673abeefacb76cc573e296ce4a94e4ee10b
jsio001 <jsio001@e.ntu.edu.sg>
2017-10-25 22:57:00 +0800

    Commit UserTweetRelation

---
+++ db/migrate/20171025145357_add_user_tweet_relation.rb
@@ -0,0 +1,5 @@
+class AddUserTweetRelation < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :tweets, :user, foreign_key: true, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025093707) do
+ActiveRecord::Schema.define(version: 20171025145357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,12 @@ ActiveRecord::Schema.define(version: 20171025093707) do
     t.text "tweet", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "reset_password_token"
@@ -38,4 +41,5 @@ ActiveRecord::Schema.define(version: 20171025093707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
