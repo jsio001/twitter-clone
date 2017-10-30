520a31f7027f23c24586dbbcb30b5e6c280dd9f6
jsio001 <jsio001@e.ntu.edu.sg>
2017-10-30 22:25:44 +0800

    fix home

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'pages#home'
 
-  resources :tweets
+  resources :tweets do
+    resources :tags, except: [:show, :index]
+  end
+
 end
