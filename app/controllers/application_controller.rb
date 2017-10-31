1000445406c3d209ad487fa5318f1d80441038ee
jsio001 <jsio001@e.ntu.edu.sg>
2017-10-31 15:43:12 +0800

    basic follow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+    def relate
+      @follow = current_user.relations.build(subscriber_id: current_user, poster_id: params[:poster_id])
+      if @follow.save
+        flash[:notice] = "Now following #{:user.username(poster_id)}"
+        redirect_to root_path
+      end
+    end
+
+    def unrelate
+    end
+
   protected
 
   def configure_permitted_parameters
