3f5dfe33ab77fb32073723abc3d77fcc5f84e360
jsio001 <jsio001@e.ntu.edu.sg>
2017-11-02 20:18:33 +0800

    model tweet_spec

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,10 @@
 class Tweet < ApplicationRecord
-  validates :tweet, presence: true
-  belongs_to :user
+
   has_many :tweet_tags, dependent: :destroy
   has_many :tags, through: :tweet_tags
+
+  belongs_to :user
+
+  validates :tweet, presence: true
+
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
 
-  it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:tweet_tags) }
   it { is_expected.to have_many(:tags) }
+
+  it { is_expected.to belong_to(:user) }
+
   it { is_expected.to validate_presence_of(:tweet)}
-  
+
 end
